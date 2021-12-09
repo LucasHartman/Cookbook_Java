2b3d42cbfa0d9a6c67caa57f02a067867ac42b1e
LucasHartman <ljh.hartman@gmail.com>
2021-12-09 14:06:01 +0100

    Exception update notes

--- 10_exception/10_knowledge/ExceptionSheet.java
+++ 10_exception/10_knowledge/ExceptionSheet.java
@@ -2,59 +2,114 @@ import java.io.FileNotFoundException;
 import java.util.function.Consumer;
 import java.io.*;
 
-/*
+public class ExceptionSheet {
 
-                                        java.lang.Throwable>
-                                                |
-                        ---------------------------------------------------
-                        |                                                  |
-                java.lang.Exception                               <Error> (Unchecked)
-                        |
+    /*
+    ----------------------------------------------------------------------------------------------------------------
+
+                                                                        Object
+                                                                            |
+                                                                        <Throwable>
+                                                                            |
+                                                    ---------------------------------------------------
+                                                    |                                                  |
+                                                <Exception>                                    <Error> (Unchecked)
+                                                    |
             ---------------------------------------------------------------------------------
             |                                       |                                       |
-    Runtime Exception> (Unchecked)          <Other Exception>                      java.io.IOException
-            |
-    - ArithmeticException                                                                   |
-    - ArrayIndexOutOfBoundsException                                       FileNotFoundException (checked Exception)
-    - ClassCastException
-    - NullPointerException
-    - IllegalArgumentException
-    - NumberFormatException
+    <Runtime Exception> (Unchecked)               (Checked)                             IOException (Checked)
+            |                                       |                                       |
+    - ArithmeticException                   - ArithmeticException                 FileNotFoundException 
+    - ArrayIndexOutOfBoundsException        - ArrayIndexOutOfBoundsException      
+    - ClassCastException                    - ClassCastException
+    - NullPointerException                  - NullPointerException
+    - IllegalArgumentException              - IllegalArgumentException
+    - NumberFormatException                 - NumberFormatException
+
+    ----------------------------------------------------------------------------------------------------------------
+
+    -- Checked exception
+    -   Checked exceptions tend to be more anticipated
+    -   Java enforeces the programmer to do something about the Checked Exepection
+        1. Handeling in the method (try/catch)
+        2. Declare in the method siganture (throws), that someone else should handle it.
+        (There are many roads to Rome, someone else can decide the best way to handel an exception)
 
 
+    -- UnChecked exception (RuntimeExceptions)
+    -   Exceptions that are thrown at runtime
+    -   unchecked exception is any exception that does not need to be
+        declared or handled by the application code
 
-    - All exceptions are subclasses of Throwable
+
+    --  Error
+    - Error represent unusual situations that are not caused by program errors and indicate
+      things that would not normally happen during program execution, such as the JVM running out of memory.
+    - Generally, your application won’t be able to recover from an Error, so you’re not required to handle them.
+    - Errors are technically not exceptions because they do not derive from class Exception.
     
-    - Java type, including Exception and RuntimeException, can be declared as the return type.
+    ----------------------------------------------------------------------------------------------------------------
 
-    - You can declare a method with RuntimeException as the return type.
+    -- throw
+    1.  throw an exception
+    2.  thow single exception not multiple
+    3.  throw only unchecked excepition
+    4.  throw is followed by the instance variable
+    5.  throw is used within the method
 
-    - You can declare any subclass of Error in the throws part of a method declaration\
 
-    - You can declare any subclass of Exception in the throws part of a method declaration.
+    -- throws
+    1.  declare an exception which might get thrown
+    2.  declare multiple exceptions
+    3.  declare checked and uncheced exception
+    4.  syntax wise throws keywrod is floolowed by expcetion class names
+    5.  throws keyword is used with the method signature
 
-    - You can declare any subclass of RuntimeException in the throws part of a method declaration.
+    ----------------------------------------------------------------------------------------------------------------
 
-    - An overridden method is allowed to throw no exceptions at all
+    -- try
+    -   try is used to define a block of code in which exceptions may occur.
+    -   This block of code is called a “guarded region” (which really means “risky code goes here”).
 
-    - 
 
-*/
+    -- catch clauses
+    -   catch clauses match a specific exception (or group of exceptions)
 
-public class ExceptionSheet {
+
+    -- finnally
+    -   clean up after ourselves if an exception occurs. 
+    -   This is the right place to close your files, release your network sockets, 
+        and perform any other cleanup your code requires.
+
+    ----------------------------------------------------------------------------------------------------------------
+
+    --  ArithmeticException
+    -   Trying to divide an int by zero gives an undefined result. When this occurs, 
+        the JVM will throw an ArithmeticException
+
+
+    --  ArrayIndexOutOfBoundsException
+    -    array indexes start with 0 and go up to 1 less
+        than the length of the array
+
+    --  ClassCastException
+    -   Java tries to protect you from impossible casts. 
+        Like an Integer to String
+
+    --  IllegalArgumentException
+    -   The code work, but you have an Integer argument,
+        but the input is a negative number.
+        In that case the programmer can throw an IllegalArgumentException
+
+    --  NullPointerException
+    -   If the reference is null, the JVM will throw a NullPointerException.
+
+    --  NumberFormatException
+
+    */
 
     public static void main(String[] args) {
 
-        // -------------------------------------------------------------------------
-        // UnChecked Exceptions: RuntimeExceptions
-        // - Exceptions that are thrown at runtime, are RuntimeExceptions
-        // - Error and RuntimeException classes are unchecked exceptions
-        // - Both runtime and checked
-        // - Exceptions can be declared ( throw new NameOfException() or throws
-        // NameOfException)
-        // - Unchecked exceptions are allowed, but not required, to be
-        // handled or declared.
-        // --------------------------------------------------------------------------
 
         Runnable arithmetic = () -> {
             // RuntimeException: Arithmetic
